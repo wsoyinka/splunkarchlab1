af8c90cfed643858fe708cf4e0289998f09b81d7
wsoyinka <wsoyinka@gmail.com>
2017-03-01 19:54:57 -0500

    indexers role

--- splunk-arch-lab-init.sh
+++ splunk-arch-lab-init.sh
@@ -20,3 +20,23 @@ ansible-playbook site.yml --tag downloadufwget,installuf   -l  forwarders
 ansible-playbook site.yml --tag set_root_dir,start_uf,start_uf_boot   -l  forwarders
 
 ansible-playbook site.yml --tag set_password1  -l  forwarders
+
+ansible-playbook site.yml --tag configure_uf_d_cli   -l  forwarders
+
+
+#### INDEXERS
+
+
+
+## indexer1
+
+sudo  /opt/splunk/bin/splunk set servername  soyinka-indexer1   -auth admin:ExpertInsight
+
+sudo  /opt/splunk/bin/splunk set default-hostname  soyinka-indexer1   -auth admin:ExpertInsight
+
+
+## indexer2
+
+sudo  /opt/splunk/bin/splunk set servername  soyinka-indexer2   -auth admin:ExpertInsight
+
+sudo  /opt/splunk/bin/splunk set default-hostname  soyinka-indexer2   -auth admin:ExpertInsight
