9cbdae88245f77efece3b0cbe2cca0bf11c53ddf
wsoyinka <wsoyinka@gmail.com>
2017-04-11 00:31:03 -0400

    Update splunk-arch-lab-init.sh

--- splunk-arch-lab-init.sh
+++ splunk-arch-lab-init.sh
@@ -46,7 +46,7 @@ case  "$1" in
       setup_searchhead()
       {
         enable_virtualenv
-        ansible-playbook site.yml --tag searchhead_role --skip-tags add_indexer1,add_indexer2 -l  searchheads
+        ansible-playbook site.yml --tag searchhead_role --skip-tags add_indexers_to_sh -l  searchheads
       }
       setup_searchhead
       ;;
