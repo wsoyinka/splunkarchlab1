e923d545b8397aa4d935a29f0bd9d9296bf31cec
wsoyinka <wsoyinka@gmail.com>
2017-04-11 15:42:31 -0400

    clean runme

--- runme.sh
+++ runme.sh
@@ -10,7 +10,3 @@ sudo yum -y install git gcc openssl-devel
 
 git clone https://github.com/wsoyinka/splunkarchlab1.git  $SPLUNKARCHLAB_BASE
 
-
-#wget -O splunk-arch-lab-init.sh  "https://raw.githubusercontent.com/wsoyinka/splunkarchlab1/master/splunk-arch-lab-init.sh"
-
-#chmod 755 splunk-arch-lab-init.sh
