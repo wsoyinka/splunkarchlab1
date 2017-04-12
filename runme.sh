12b4f3fc562ae439d6c21c2773a3aa80e9858b7e
wsoyinka <wsoyinka@gmail.com>
2017-04-12 13:31:35 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -6,7 +6,7 @@ SPLUNKARCHLAB_BASE=splunkarchlab_base
 
 rm -rf splunk*
 
-sudo yum -y install tmux git gcc openssl-devel virtualenv
+sudo yum -y install tmux git gcc openssl-devel python26-virtualenv
 
 git clone https://github.com/wsoyinka/splunkarchlab1.git  $SPLUNKARCHLAB_BASE
 
