f1fa9808f01f659b3abeecfe9b3d728c697c34f1
wsoyinka <wsoyinka@gmail.com>
2017-04-12 11:43:50 -0400

    Update splunk-arch-lab-init.sh

--- splunk-arch-lab-init.sh
+++ splunk-arch-lab-init.sh
@@ -106,7 +106,7 @@ case  "$1" in
        ansible-playbook site.yml --tag run_diag -l  searchheads
        ansible-playbook site.yml --tag run_diag2 -l  searchheads
        ansible-playbook site.yml --tag run_diag -l forwarders
-       ansible-playbook site.yml --tag run_diag2 -l forwarders
+       #ansible-playbook site.yml --tag run_diag2 -l forwarders
        ansible-playbook site.yml --tag run_diag -l indexers
        ansible-playbook site.yml --tag run_diag2 -l indexers
      }
